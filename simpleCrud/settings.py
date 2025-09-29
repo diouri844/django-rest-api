51fd816003b72d793549b4586848eab2a863bccd
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-09-29 19:40:00 +0200

    update the main core app

--- simpleCrud/settings.py
+++ simpleCrud/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     "pools.apps.PoolsConfig",
     'rest_framework',
     'corsheaders',
+    "users",
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -83,6 +85,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
 
