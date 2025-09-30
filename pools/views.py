5b1e849aaa21083c3c0019a2f81b5df68d19bf52
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-09-30 10:59:17 +0200

    move unused apps

--- pools/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-# Create your views here.
-
-def index(request):
-    print("App is running under the pools app tara ..... ")
-    return HttpResponse("Hello, world. You're at the polls index.")
