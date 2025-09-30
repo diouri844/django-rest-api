86e5e16e5ce7eefa12fc35f98ad3d64fbd2c0a7d
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-09-30 11:25:14 +0200

    update user app urls to include create_customer and list_customers paths

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('create_customer/', views.create_customer, name='create_customer'),
+    path('list_customers/', views.list_customers, name='list_customers'),
     path('login/', views.login, name='login'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout, name='logout'),
