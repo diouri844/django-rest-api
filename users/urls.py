53adf87a3742f0a72bd25bedfc0948f9bfde6882
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-09-30 11:00:39 +0200

    update user app urls

--- users/urls.py
+++ users/urls.py
@@ -2,12 +2,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
-from .apps import AuthConfig
 
 urlpatterns = [
-    path(AuthConfig.prefix +'register/', views.register, name='register'),
+    path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout, name='logout'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
