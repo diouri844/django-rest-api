7e03b68c3d0646b2ddd8ae63b1a6c4f381156ccc
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-09-30 10:59:42 +0200

    update settings

--- simpleCrud/settings.py
+++ simpleCrud/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "pools.apps.PoolsConfig",
     'rest_framework',
     'corsheaders',
     "users",
